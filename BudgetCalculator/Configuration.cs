18334895da45d80011f2ed6ad4edce63b7afeeb8
Naviras93 <ivan.saric1@hotmail.com>
2020-08-22 18:50:00 +0200

    Initial commit with all of the necessary items

---
+++ BudgetCalculator/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator
+{
+	public class Configuration
+	{
+		public List<string> ExcelFilesToRead { get; set; }
+		public List<ExportCategory> ExportCategories { get; set; }
+		public string CategoryOthersName { get; set; }
+		public bool ListUpPurchaseSourcesInOthers { get; set; }
+		public string ExportFileName { get; set; }
+		public string ExportPathName { get; set; }
+
+		public class ExportCategory
+		{
+			public string CategoryName { get; set; }
+			public List<string> PurchaseSourceNames { get; set; }
+		}
+	}
+}
--- BudgetCalculator/Program.cs
+++ BudgetCalculator/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,16 @@ namespace BudgetCalculator
 	{
 		static void Main(string[] args)
 		{
+			var jsonString = File.ReadAllText(Directory.GetCurrentDirectory() + "configuration.json");
+			var configuration = JsonConvert.DeserializeObject<Configuration>(jsonString);
+
+
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			using (var package = new ExcelPackage(new FileInfo("MyWorkbook.xlsx")))
+			{
+
+			}
 		}
 	}
 }
